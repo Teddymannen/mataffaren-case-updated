ccc9e664805f3a99164163f10c4a5fc28e734aaf
lelonS <35070147+lelonS@users.noreply.github.com>
2023-11-09 10:02:15 +0100

    Files

---
+++ tests/helpers/index.js
@@ -0,0 +1,30 @@
+const baseUrl = 'http://localhost:4000';
+
+const sortingInfo = {
+  'topRated': { sortParameter: 'ranking', 'asc': false },
+  'name-asc': { sortParameter: 'name', 'asc': true },
+  'name-desc': { sortParameter: 'name', 'asc': false },
+  'price-asc': { sortParameter: 'priceValue', 'asc': true },
+  'price-desc': { sortParameter: 'priceValue', 'asc': false },
+  'compareprice-asc': { sortParameter: 'comparePrice', 'asc': true },
+  'compareprice-desc': { sortParameter: 'comparePrice', 'asc': false }
+};
+
+
+
+/**
+ * Get category and all child categories in a single list, recursively
+ * @param category - A category that has a list of children
+ * @returns {Array} All categories in an array
+ */
+function getAllCategories(category) {
+  const categories = [category];
+  for (const childCategory of category.children) {
+    const childCategories = getAllCategories(childCategory);
+    categories.push(...childCategories);
+  }
+  return categories;
+}
+
+
+export { baseUrl, getAllCategories, sortingInfo };
